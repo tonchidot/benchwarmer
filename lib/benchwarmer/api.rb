b074d366fc38f538758bf5838a7dc6fb52cbd9b1
arai hiroki <arai@tonchidot.com>
2013-11-14 16:50:47 +0900

    work around for issue of XMLRPC (http://bugs.ruby-lang.org/issues/8182)

--- lib/benchwarmer/api.rb
+++ lib/benchwarmer/api.rb
@@ -2,9 +2,9 @@ module Benchwarmer
   class API
     # Blank Slate
     instance_methods.each do |m|
-      undef_method m unless m.to_s =~ /^__|object_id|method_missing|respond_to?|to_s|inspect/
+      undef_method m unless m.to_s =~ /^__|object_id|method_missing|respond_to\?|to_s|inspect|instance_eval/
     end
-    
+
     # Benchmark Email API Documentation: http://www.benchmarkemail.com/API/Library
     BENCHMARK_API_VERSION = "1.0"
     DEFAULTS = {
@@ -17,6 +17,7 @@ module Benchwarmer
     def initialize(api_token, config = {})
       opts = DEFAULTS.merge(config).freeze
       @api_client = XMLRPC::Client.new2("#{opts[:secure] ? 'https' : 'http'}://api.benchmarkemail.com/#{opts[:api_version]}/", nil, opts[:timeout])
+      @api_client.http_header_extra = { 'accept-encoding' => 'identity' }
       @api_token = api_token
     end
 
@@ -26,13 +27,13 @@ module Benchwarmer
       super if error.message.include?("unsupported method called:")
       raise APIError.new(error)
     end
-    
+
     def respond_to?(api_method) # :nodoc:
       @api_client.call(api_method, @api_token)
     rescue XMLRPC::FaultException => error
       error.message.include?("unsupported method called:") ? false : true
     end
-    
+
     # Token management methods
     #
     # Each time you "login", a new token is generated with access to the B.E. API.
@@ -48,12 +49,12 @@ module Benchwarmer
     #   "API tokens" are currently used.
     #
     # In other words, make it more like Mailchimp.
-    
+
     def self.login(username, password, config = {})
       api_client(config)
       @api_client.call('login', username, password)
     end
-    
+
     # Get a list of the tokens
     def self.token_get(username, password, config = {})
       api_client(config)
@@ -71,20 +72,22 @@ module Benchwarmer
       api_client(config)
       @api_client.call("tokenDelete", username, password, token)
     end
-    
+
     private
-    
+
     def self.api_client(options)
       opts = DEFAULTS.merge(options).freeze
       @api_client = XMLRPC::Client.new2("#{opts[:secure] ? 'https' : 'http'}://api.benchmarkemail.com/#{opts[:api_version]}/", nil, opts[:timeout])
+      @api_client.http_header_extra = { 'accept-encoding' => 'identity' }
+      @api_client
     end
-    
+
     def camelize_api_method_name(str)
       str.to_s[0].chr.downcase + str.gsub(/(?:^|_)(.)/) { $1.upcase }[1..str.size]
     end
-    
+
   end
-  
+
   class APIError < StandardError
     def initialize(error)
       super("(#{error.faultCode}) #{error.message}")
--- spec/benchwarmer_spec.rb
+++ spec/benchwarmer_spec.rb
@@ -10,25 +10,39 @@ module Benchwarmer
     it "also accepts config options on initialization" do
       expect {Benchwarmer::API.new('api_token', {:secure => true})}.should_not raise_error(ArgumentError)
     end
-    
+
     it "requires a username and password for class methods" do
       expect {Benchwarmer::API.login('password')}.should raise_error(ArgumentError)
       expect {Benchwarmer::API.token_get('password')}.should raise_error(ArgumentError)
       expect {Benchwarmer::API.token_add('password')}.should raise_error(ArgumentError)
       expect {Benchwarmer::API.token_delete('password')}.should raise_error(ArgumentError)
     end
-    
+
     it "requires a token to delete" do
       expect {Benchwarmer::API.token_delete('username', 'password')}.should raise_error(ArgumentError)
     end
-    
+
     it "also accepts config options on class methods" do
       expect {Benchwarmer::API.login('username', 'password', {:secure => true})}.should_not raise_error(ArgumentError)
       expect {Benchwarmer::API.token_get('username', 'password', {:secure => true})}.should_not raise_error(ArgumentError)
       expect {Benchwarmer::API.token_add('username', 'password', {:secure => true})}.should_not raise_error(ArgumentError)
-      expect {Benchwarmer::API.token_delete('username', 'password', 'token', {:secure => true})}.should_not raise_error(ArgumentError)  
+      expect {Benchwarmer::API.token_delete('username', 'password', 'token', {:secure => true})}.should_not raise_error(ArgumentError)
     end
-    
+
+    describe 'Accept-Encoding header' do
+      shared_examples_for 'set Accept-Encoding: identity' do
+        it { subject.http_header_extra["accept-encoding"].should eq "identity" }
+      end
+
+      it_behaves_like 'set Accept-Encoding: identity' do
+        subject { Benchwarmer::API.new('api_token', {:secure => true}).instance_eval { @api_client } }
+      end
+
+      it_behaves_like 'set Accept-Encoding: identity' do
+        subject { Benchwarmer::API.api_client({}) }
+      end
+    end
+
     # TODO: Need a way to mock XMLRPC requests.
 
   end
